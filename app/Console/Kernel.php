c38999bc0c2dab283237d95fd03aa082753be7df
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-06-24 16:30:39 +0200

    [#7] Add command 'harvest:info'
    Loops through the harvested records, parses MARC21 using SimpleMarcParser, and prints some summary information.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Colligator\Console\Commands\OaiPmhHarvest::class,
+        \Colligator\Console\Commands\OaiPmhHarvestInfo::class,
     ];
 
     /**
