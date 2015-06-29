75cfc9e6e9a929e9aef5b90dea70b85098339dd4
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-06-29 11:38:11 +0200

    [#13] Disable Laravel CsrfToken middleware
    Won't work for requests from outside Laravel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Colligator\Http\Middleware\VerifyCsrfToken::class,
+        // \Colligator\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
