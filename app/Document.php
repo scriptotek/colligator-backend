19e8f647bb15255e940d31f170105e009d0947f6
Dan Michael O. Heggø <danmichaelo@gmail.com>
2015-06-29 21:40:09 +0000

    Add 'fillable' to the Document model

--- app/Document.php
+++ app/Document.php
@@ -13,6 +13,13 @@ class Document extends Model
 {
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['bibsys_id', 'bibliographic', 'holdings'];
+
+    /**
      * The attributes that should be casted to native types.
      *
      * @var array
