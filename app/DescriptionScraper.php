d8bbc263c4347db5370c94c8c605ef75d736ce92
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-07-04 19:42:49 +0200

    Remove Slack logging

--- app/DescriptionScraper.php
+++ app/DescriptionScraper.php
@@ -35,17 +35,6 @@ class DescriptionScraper
         }
     }
 
-    public function notify($msg, $doc, $args, $level = 'warning')
-    {
-        $msg = vsprintf($msg, $args);
-        $docLink = sprintf('<http://colligator.biblionaut.net/api/documents/%s|#%s> ', $doc->id, $doc->id);
-        \Slack::attach([
-            'fallback' => '#' . $doc->id . ' ' . $msg,
-            'text'     => $docLink . $msg,
-            'color'    => $level,
-        ])->send();
-    }
-
     public function scrape($url)
     {
         foreach ($this->scrapers as $scraper) {
@@ -77,19 +66,13 @@ class DescriptionScraper
             $result = $this->scrape($url);
         } catch (TransferException $e) {
             \Log::error('[DescriptionScraper] Transfer failed: ' . $e->getMessage());
-            $this->notify('*DescriptionScraper* failed to fetch: %s', $doc, [$url]);
-
             return;
         } catch (Scrapers\ScrapeException $e) {
             \Log::error('[DescriptionScraper] Scraping of ' . $url . ' failed: ' . $e->getMessage());
-            $this->notify('*DescriptionScraper* / %s failed to find a text at: %s', $doc, [$e->getMessage(), $url]);
-
             return;
         }
         if (is_null($result)) {
             \Log::error('Encountered URL not recognized by any scraper: ' . $url);
-            $this->notify('*DescriptionScraper* encountered URL not recognized by any sraper: %s', $doc, [$url]);
-
             return;
         }
 
