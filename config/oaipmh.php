180bc48fc0482d058704742579ab28682999ae79
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-07-05 00:33:21 +0200

    Harvest ureal_scifi collection
    Fixes #6

--- config/oaipmh.php
+++ config/oaipmh.php
@@ -14,7 +14,6 @@ return [
 
     'harvests' => [
         'samling42' => [
-            // 'url'         => 'https://sandbox-eu.alma.exlibrisgroup.com/view/oai/47BIBSYS_UBO/request',
             'url'         => 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_UBO/request',
             'set'         => 'ureal_samling42',
             'schema'      => 'marc21',
@@ -23,6 +22,12 @@ return [
         's-litt' => [
             'url'         => 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_UBO/request',
             'set'         => 'S-Litt_Colligator',
+            'schema'      => 'marc21',
+            'max-retries' => 10,
+        ],
+        'scifi' => [
+            'url'         => 'https://bibsys.alma.exlibrisgroup.com/view/oai/47BIBSYS_UBO/request',
+            'set'         => 'ureal_scifi',
             'schema'      => 'marc21',
             'max-retries' => 10,
         ],
